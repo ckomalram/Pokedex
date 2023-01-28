412f83e9a7b69d149b6f8ae069d4bba69dbdf773
ckomalram <glaw14@gmail.com>
2023-01-27 22:17:41 -0500

    + proyecto inicial con navigation

---
+++ src/screens/Favorite.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View , SafeAreaView} from 'react-native'
+
+export default class Favorite extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text> Favorte screens </Text>
+      </SafeAreaView>
+    )
+  }
+}
---
+++ src/screens/Pokedex.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class Pokedex extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Pokedex screen </Text>
+      </View>
+    )
+  }
+}
