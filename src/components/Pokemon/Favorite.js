e1797856c0faaa7770b9b44816fcd7898af20763
ckomalram <glaw14@gmail.com>
2023-01-29 23:04:30 -0500

    Agregando estados para controlar favoritos

--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,21 +1,57 @@
-import React from 'react';
+import React , {useEffect , useState} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-import { addPokemonFavoriteApi , getPokemonsFavoriteApi } from "../../api/Favorite";
+import { addPokemonFavoriteApi , isPokemonFavoriteApi , removePokemonFavoriteApi } from "../../api/Favorite";
 
 export default function Favorite(props) {
     const { id} = props;
 
+    const [isFavorite, setIsFavorite] = useState(undefined);
+    const [reloadCheck, setReloadCheck] = useState(false);
+
+    const Icon = isFavorite ? 'heart' : 'heart-outline';
+
+    useEffect(() => {
+        (async () => {
+          try {
+            const response = await isPokemonFavoriteApi(id);
+            setIsFavorite(response);
+          } catch (error) {
+            setIsFavorite(false);
+          }
+        })();
+      }, [id,reloadCheck]);
+    
+      const onReloadCheckFavorite = () => {
+        setReloadCheck((prev) => !prev);
+      };
+
+
+
     const addFavorite = async () => {
         console.log('Agregando favorito');
         await addPokemonFavoriteApi(id);
+        onReloadCheckFavorite();
+
       };
+
+      const removeFavorite = async () => {
+        try {
+            console.log('Removiendo...');
+          await removePokemonFavoriteApi(id);
+          onReloadCheckFavorite();
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+
     return (
         <Ionicons
-            name='heart-outline'
+            name={Icon}
             color='#fff'
             size={20}
-            onPress={ addFavorite}
+            onPress={isFavorite ? removeFavorite : addFavorite}
         />
     )
 }
